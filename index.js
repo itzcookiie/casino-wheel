db9fac779878e2f5e7001957479bc0a8009499ff
itzcookiie <mayowaadeniyi@hotmail.com>
2021-01-22 17:18:51 +0000

    Added title and add UI for clicking on lever

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const listOfWords = document.querySelector('.list-of-words'),
-lever = document.querySelector('.lever');
+lever = document.querySelector('.lever'),
+leverHandle = document.querySelector('.lever__handle');
 
 const words = ['tax','suit','weave','noise','swop','stuff','medal','rhythm','pardon','attractive'];
 let arrangedWords = [];
 
-lever.addEventListener('click', () => {
+let spinTimeMS;
+
+leverHandle.addEventListener('click', e => {
+    e.target.setAttribute('disabled', '');
+    e.target.classList.toggle('moveLever');
     spin();
+    const leverTime = setTimeout(() => {
+        clearTimeout(leverTime);
+        const leverUpTime = setTimeout(() => {
+            clearTimeout(leverUpTime);
+            e.target.removeAttribute('disabled');
+            e.target.classList.toggle('moveLever');
+        }, spinTimeMS - 500)
+    }, 500)
 })
 
 function addWordsToDOM(wordsList) {
@@ -15,12 +28,18 @@ function addWordsToDOM(wordsList) {
     const firstThreeWords = wordsList.slice(0, 2);
     firstThreeWords.map((word, index) => {
         const liElement = document.createElement('li');
+        if(index === firstThreeWords.length - 1) {
+            liElement.innerHTML = 
+            `
+                <h2>${word}</h2>
+            `;
+            liElement.classList.add('display');
+        }
         liElement.innerHTML = 
         `
-            <p>${word}</p>
+            <h4>${word}</h4>
         `;
         liElement.classList.add('word');
-        if(index === firstThreeWords.length - 1) liElement.classList.add('display');
         fragment.appendChild(liElement);
     })
     listOfWords.appendChild(fragment);
@@ -41,14 +60,24 @@ function getWords() {
 }
 
 function spin() {
+    setSpinTime();
+    const randomTime = (Math.random() * 5) * 1000;
     const spinID = setInterval(() => {
         addWordsToDOM(shuffleWords(getWords()));
-    }, 200)
+    }, 100)
 
     const spinTimer = setTimeout(() => {
         clearInterval(spinID);
         clearTimeout(spinTimer);
-    }, 5000)
+    }, spinTimeMS)
+}
+
+function spinTime(multiplier=5) {
+    return parseInt((Math.random() * multiplier) * 1000);
+}
+
+function setSpinTime() {
+    spinTimeMS = spinTime();
 }
 
 addWordsToDOM(words)
